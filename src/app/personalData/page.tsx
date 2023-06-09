7aa09e651e61fe6a88b349f0b9b33a50ee846da5
Julio Macedo <juliocesar1076@gmail.com>
2023-06-09 15:33:37 -0300

    feat: create personal data register

---
+++ src/app/personalData/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+import { SubTitle } from '../components/Subtitle';
+import { Title } from '../components/Title';
+import { HelpFlixLogo } from '../components/logo';
+
+export default function PersonalData() {
+  return (
+    <main className="mt-10 flex flex-col w-full mb-5">
+      <Title title="Dados pessoais" />
+      <SubTitle subTitle="Adicione seus dados abaixo" />
+      <div className="flex gap-5 flex-col">
+        <Input placeHolder="Nome" inputName="Nome" required />
+        <Input placeHolder="Sobrenome" inputName="Sobrenome" required />
+        <Input placeHolder="Telefone" inputName="Telefone" required />
+        <Input placeHolder="Email" inputName="Email" required />
+        <Input
+          placeHolder="Data de nascimento"
+          inputName="Data de nascimento"
+          required
+        />
+        <Button name="Avançar" route="" />
+      </div>
+      <HelpFlixLogo />
+    </main>
+  );
+}
