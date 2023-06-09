10db64d70736fa16fe6a663ea32569dbbee8bd3e
Julio Macedo <juliocesar1076@gmail.com>
2023-06-09 15:34:13 -0300

    feat: add link in button

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import Link from 'next/link';
 
 interface ButtonProps {
   name: string;
+  route: string;
 }
 
-export const Button = ({ name }: ButtonProps) => {
+export const Button = ({ name, route }: ButtonProps) => {
   return (
     <button
       type="submit"
       className="h-14 bg-gradient-to-r from-secondary-800 to-secondary-400 rounded-lg text-lg font-bold text-white"
     >
-      {name}
+      <Link className="h-full w-full" href={route}>
+        {name}
+      </Link>
     </button>
   );
 };
