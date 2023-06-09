d2b1b19b7734c328df3ac2fca40744cf19a4f3c1
Julio Macedo <juliocesar1076@gmail.com>
2023-06-09 16:33:34 -0300

    feat: add margin bottom

--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -4,5 +4,7 @@ interface TitleProps {
   title: string;
 }
 export const Title = ({ title }: TitleProps) => {
-  return <h2 className="text-2xl font-bold text-black text-center">{title}</h2>;
+  return (
+    <h2 className="text-2xl font-bold text-black text-center mb-5">{title}</h2>
+  );
 };
