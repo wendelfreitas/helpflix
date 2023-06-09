e5565ae77a598dbf168fd5420c61360561d4bb6b
Julio Macedo <juliocesar1076@gmail.com>
2023-06-09 11:41:31 -0300

    feat: toggle switch in p tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,14 +80,21 @@ export default function Home() {
         className="border border-light-grey h-11 rounded-lg placeholder-light-grey px-4"
         placeholder="Nome"
       />
-      <div className="py-5 flex gap-6">
-        <label className="relative items-center cursor-pointer ">
-          <input type="checkbox" value="" className="sr-only peer" />
-          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      <div className="py-5 ">
+        <label htmlFor="switch" className="flex gap-6">
+          <div className="relative items-center cursor-pointer ">
+            <input
+              id="switch"
+              type="checkbox"
+              value=""
+              className="sr-only peer"
+            />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </div>
+          <p className="text-sm text-dark-grey">
+            Desejo doar apenas uma vez, não assinar.
+          </p>
         </label>
-        <p className="text-sm text-dark-grey">
-          Desejo doar apenas uma vez, não assinar.
-        </p>
       </div>
       <button
         type="submit"
