b2d328e623a34b6d8d2aa7420ef3da588b46c23c
Julio Macedo <juliocesar1076@gmail.com>
2023-06-09 11:07:01 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from 'next/image'
+import logo from '../../static/assets/img/logo.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="mt-12 flex flex-col">
+      <h2 className="text-2xl font-bold text-black text-center">
+        Faça sua doação
+      </h2>
+      <p className="text-center text-dark-grey text-sm mt-7 mb-10">
+        Faça a assinatura do bem e ajude o Pão do Povo da Rua mensalmente!
+      </p>
+      <ul className="flex gap-5 justify-center pb-10">
+        <li>
+          <input
+            type="radio"
+            id="deposit10"
+            name="radioCard"
+            value="10"
+            className="hidden peer"
+            required
+          />
+          <label
+            htmlFor="deposit10"
+            className="flex items-center flex-col w-24 h-24 p-3 text-medium-gray text-sm bg-white border-2 border-light-grey rounded-lg cursor-pointer  peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <p className="text-center">Assine</p>
+            <span className="text-base text-black text-center font-bold peer-checked:text-blue-600">
+              10 Reais
+            </span>
+            <p className="text-center">por mês</p>
+          </label>
+        </li>
+        <li>
+          <input
+            type="radio"
+            id="deposit30"
+            name="radioCard"
+            value="30"
+            className="hidden peer"
+            required
+          />
+          <label
+            htmlFor="deposit30"
+            className="flex items-center flex-col w-24 h-24 p-3 text-medium-gray text-sm bg-white border-2 border-light-grey rounded-lg cursor-pointer  peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
+          >
+            <p className="text-center">Assine</p>
+            <span className="text-base text-black text-center font-bold peer-checked:text-blue-600">
+              30 Reais
+            </span>
+            <p className="text-center">por mês</p>
+          </label>
+        </li>
+        <li>
+          <input
+            type="radio"
+            id="deposit50"
+            name="radioCard"
+            value="50"
+            className="hidden peer"
+            required
+          />
+          <label
+            htmlFor="deposit50"
+            className="flex items-center flex-col w-24 h-24 p-3 text-medium-gray text-sm bg-white border-2 border-light-grey rounded-lg cursor-pointer  peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
+          >
+            <p className="text-center">Assine</p>
+            <span className="text-base text-black text-center font-bold peer-checked:text-blue-600">
+              50 Reais
+            </span>
+            <p className="text-center">por mês</p>
+          </label>
+        </li>
+      </ul>
+      <p className="text-dark-grey text-md mb-2  ">
+        Caso prefira, digite o valor desejado para a assinatura:
+      </p>
+      <input
+        type="text"
+        className="border border-light-grey h-11 rounded-lg placeholder-light-grey px-4"
+        placeholder="Nome"
+      />
+      <div className="py-5 flex gap-6">
+        <label className="relative items-center cursor-pointer ">
+          <input type="checkbox" value="" className="sr-only peer" />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        </label>
+        <p className="text-sm text-dark-grey">
+          Desejo doar apenas uma vez, não assinar.
+        </p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <button
+        type="submit"
+        className="h-14 bg-gradient-to-r from-secondary-800 to-secondary-400 rounded-lg text-lg font-bold text-white"
+      >
+        Avançar
+      </button>
+      <div className="flex gap-2 my-5  justify-center">
+        <p className="text-xs text-medium-gray">Power by</p>
+        <Image src={logo} width={85} height={10} alt=" helpflix logo" />
       </div>
     </main>
-  )
+  );
 }
