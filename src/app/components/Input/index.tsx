9fc62094e4afc1c0ab8e6a0160905646c8acdbc4
Julio Macedo <juliocesar1076@gmail.com>
2023-06-12 15:45:33 -0300

    refactoring: change prop name

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -3,20 +3,26 @@ import React from 'react';
 interface InputProps {
   placeHolder: string;
   required?: boolean;
-  inputName: string;
+  label: string;
 }
-export const Input = ({ placeHolder, inputName, required }: InputProps) => {
+export const Input = ({ placeHolder, label, required }: InputProps) => {
   return (
     <div className="w-full">
       <div className="flex items-center">
         {required && (
           <span className="text-medium-gray text-base font-medium">*</span>
         )}
-        <p className="text-medium-gray text-base font-medium">{inputName}</p>
+        <label
+          htmlFor={label}
+          className="text-medium-gray text-base font-medium"
+        >
+          {label}
+        </label>
       </div>
 
       <input
         type="text"
+        id={label}
         className="border border-light-grey h-11 w-full rounded-lg placeholder-light-grey px-4"
         placeholder={placeHolder}
       />
