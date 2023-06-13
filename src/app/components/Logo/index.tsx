65b9c87b9a200305edc202b63d763e8bde74f05a
Julio Macedo <juliocesar1076@gmail.com>
2023-06-13 09:34:57 -0300

    feat: add className prop in component

--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from '../../../../public/img/logo.svg';
 import Image from 'next/image';
 
-export const HelpFlixLogo = () => {
+interface HelpFlixLogoProps {
+  className: string;
+}
+
+export const HelpFlixLogo = ({ className }: HelpFlixLogoProps) => {
   return (
-    <div className="flex gap-2 mt-5  justify-center">
+    <div className={`flex gap-2 justify-center ${className}`}>
       <p className="text-xs text-medium-gray">Power by</p>
       <Image src={logo} width={85} height={10} alt=" helpflix logo" />
     </div>
