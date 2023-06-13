417e4ed283e85695a1fbcb45fbe09e5e9fa3c693
Julio Macedo <juliocesar1076@gmail.com>
2023-06-13 09:35:09 -0300

    feat: add className prop in component

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link';
 interface ButtonProps {
   name: string;
   route: string;
+  className?: string;
 }
 
-export const Button = ({ name, route }: ButtonProps) => {
+export const Button = ({ name, route, className }: ButtonProps) => {
   return (
     <button
       type="submit"
-      className="h-14 bg-gradient-to-r from-secondary-800 to-secondary-400 rounded-lg text-lg font-bold text-white"
+      className={`h-14 bg-gradient-to-r from-secondary-800 to-secondary-400 rounded-lg text-lg font-bold text-white ${className}`}
     >
       <Link className="h-full w-full" href={route}>
         {name}
