43251ffbaa9eb6f95d58fa84f95908a71b7d8239
Julio Macedo <juliocesar1076@gmail.com>
2023-06-13 09:33:17 -0300

    feat: add props

--- src/app/components/Subtitle/index.tsx
+++ src/app/components/Subtitle/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 interface SubTitleProps {
   subTitle: string;
+  className?: string;
 }
 
-export const SubTitle = ({ subTitle }: SubTitleProps) => {
-  return <p className="text-center text-dark-grey text-sm mb-5">{subTitle}</p>;
+export const SubTitle = ({ subTitle, className }: SubTitleProps) => {
+  return (
+    <p className={`text-center text-dark-grey text-sm ${className}`}>
+      {subTitle}
+    </p>
+  );
 };
--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 interface TitleProps {
   title: string;
+  className?: string;
 }
-export const Title = ({ title }: TitleProps) => {
+export const Title = ({ title, className }: TitleProps) => {
   return (
-    <h2 className="text-2xl font-bold text-black text-center mb-5">{title}</h2>
+    <h2 className={`text-2xl font-bold text-black text-center ${className}`}>
+      {title}
+    </h2>
   );
 };
