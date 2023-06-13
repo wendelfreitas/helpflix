a0a5803fe6cd3548bd69a0505cdd6b7657852a18
Julio Macedo <juliocesar1076@gmail.com>
2023-06-13 16:39:24 -0300

    feat : add margins

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -5,26 +5,27 @@ import { Title } from '../components/Title';
 import { HelpFlixLogo } from '../components/Logo';
 import { DonateCardValue } from '../components/DonateCardValue';
 import { SubTitle } from '../components/Subtitle';
-import { LottieComponent } from '../components/Lottie/Lottie';
+import { LottieComponent } from '../components/Lottie';
 
 export default function Address() {
   return (
-    <main className="mt-10 flex flex-col w-full mb-5">
-      <Title title="Deu tudo certo!" />
+    <main className="flex flex-col w-full mb-5">
       <LottieComponent
         animationData={checkSucces}
         style={{ height: 88, wdith: 88 }}
       />
-      <SubTitle subTitle="Agora você contribui" />
+      <Title className="mt-7" title="Deu tudo certo!" />
+
+      <SubTitle className="my-7" subTitle="Agora você contribui" />
       <DonateCardValue value="50" />
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-8 mb-5">
         <Image src={emailIcon} width={62} height={62} alt="email icon" />
       </div>
       <p className="text-center text-sm text-dark-grey">
         Você irá receber um email com a confirmação de pagamento e o cadastro no
         site helpflix.com.br para gerenciar seus pagamentos.
       </p>
-      <HelpFlixLogo />
+      <HelpFlixLogo className="mt-8" />
     </main>
   );
 }
